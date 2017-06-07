fc2153512b5c7fd22ba3ee6009e018ca4b34cf7b
YannickOlivier <yannick.olivier2@gmail.com>
2017-06-07 23:37:10 +0200

    UP Network folder

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ var chokidar = require('chokidar');
 
 
 // watcher
-chokidar.watch('.', {
-    ignored: /(^|[\/\\])\../,
+watcher = chokidar.watch('//MIBOX3_5378/internal storage/Download', {
+    persistent: true,
+    ignoreInitial: true,
+    usePolling: true,
     awaitWriteFinish: {
       stabilityThreshold: 100,
       pollInterval: 100
-    },
-  }).on("all", (event, path) => {
-  console.log(event, path);
+    }
+  });
+
+
+watcher.on('all', function(path) {
+  console.log(path, 'added')
   });
